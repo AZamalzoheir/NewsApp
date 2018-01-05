9696d0c6b0f22669152f43c1fc0669a8a2f3bcb5
AZamalzoheir <A.Zamalzoheir95@gamil.com>
2018-01-05 13:17:21 +0200

    second commit

--- app/src/main/java/com/example/amalzoheir/newsapp/MainActivity.java
+++ app/src/main/java/com/example/amalzoheir/newsapp/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.amalzoheir.newsapp;
 import android.app.LoaderManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v4.content.Loader;
+import android.support.v4.content.SharedPreferencesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements LoaderCallbacks<ArrayList<NewsInfo>>{
     public static final String LOG_TAG = MainActivity.class.getName();
-    public static final String API_REQUEST_URL ="http://content.guardianapis.com/search?q=movies&api-key=test&show-tags=contributor";
+    public static final String API_REQUEST_URL ="http://content.guardianapis.com/search?";
     private NewsInfoAdapter madapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +31,32 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<A
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.action_settings){
+            Intent settingsIntent=new Intent(this,SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public android.content.Loader<ArrayList<NewsInfo>> onCreateLoader(int id, Bundle args) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String news= sharedPrefs.getString(getString(R.string.settings_story_key),getString(R.string.settings_story_default));
+        Uri baseUri = Uri.parse(API_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("q",news);
+        uriBuilder.appendQueryParameter("api-key","test");
+        uriBuilder.appendQueryParameter("show-tag","contributor");
         return new NewsInfoLoader(this,API_REQUEST_URL);
     }
 
---
+++ app/src/main/java/com/example/amalzoheir/newsapp/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.example.amalzoheir.newsapp;
+
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+    public static class NewsInfoPreferenceFragment extends PreferenceFragment{
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.xml);
+        }
+    }
+}
